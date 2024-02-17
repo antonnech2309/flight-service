764b54037f19a92e8548e4157ce848f5a340e8f9
Anton <anton380969658858@gmail.com>
2024-02-17 19:42:35 +0200

    add order, route, flight,ticket models to airport

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -24,3 +25,54 @@ class Airplane(models.Model):
 class Crew(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="orders"
+    )
+
+
+class Route(models.Model):
+    source = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    distance = models.IntegerField()
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(
+        Route,
+        on_delete=models.CASCADE
+    )
+    airplane = models.ForeignKey(
+        Airplane,
+        on_delete=models.CASCADE
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def __str__(self):
+        return (f"{self.airplane.name}-"
+                f"{self.departure_time}:{self.arrival_time}")
+
+    class Meta:
+        default_related_name = "flights"
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(
+        Flight,
+        on_delete=models.CASCADE
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        default_related_name = "tickets"
