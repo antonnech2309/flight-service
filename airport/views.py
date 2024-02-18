abb733e6bf688317efac580b70980f5755a9bfe4
Anton <anton380969658858@gmail.com>
2024-02-18 16:49:53 +0200

    create endpoint for airport model

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from airport.models import Airport
+from airport.serializers import AirportSerializer
 
 
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
