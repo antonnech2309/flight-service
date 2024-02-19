83e5f9fae77c3903095e081e36a3d1b78a4cdf75
Anton <anton380969658858@gmail.com>
2024-02-19 13:41:05 +0200

    create endpoint for airplane type model

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from airport.models import Airport
+from airport.models import Airport, AirplaneType
 
 
 class AirportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Airport
         fields = ["id", "name", "closest_big_city"]
+
+
+class AirplaneTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AirplaneType
+        fields = ["id", "name"]
--- airport/urls.py
+++ airport/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from airport.views import AirportViewSet
+from airport.views import AirportViewSet, AirplaneTypeViewSet
 
 router = routers.DefaultRouter()
 router.register("airports", AirportViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
