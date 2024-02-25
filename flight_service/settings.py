07bae27e2baa106a94c5c12241ae5a9e664cafda
Anton <anton380969658858@gmail.com>
2024-02-25 14:20:03 +0200

    add throtling to the project

--- flight_service/settings.py
+++ flight_service/settings.py
@@ -147,4 +147,14 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "5/minute",
+        "user": "10/minute"
+    }
 }
